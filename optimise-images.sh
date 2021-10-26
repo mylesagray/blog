fbff026b4c678728fe2b0287b2998a07b7eff26e
Myles G <git.ii3ln@slmail.me>
2021-10-26 18:00:06 +0100

    Made images in posts a figure and allow for caption, updated script to max-size the width of all images to 740px

--- optimise-images.sh
+++ optimise-images.sh
@@ -1,6 +1,12 @@
-find ./ -type f -name '*.png' -exec sh -c 'cwebp $1 -o "${1%.png}.webp"' _ {} \;
-find ./ -type f -name '*.jpg' -exec sh -c 'cwebp $1 -o "${1%.jpg}.webp"' _ {} \;
-find ./ -type f -name '*.jpeg' -exec sh -c 'cwebp $1 -o "${1%.jpeg}.webp"' _ {} \;
-find ./ -type f -name '*.png' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.png}.avif"' _ {} \;
-find ./ -type f -name '*.jpg' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.jpg}.avif"' _ {} \;
-find ./ -type f -name '*.jpeg' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.jpeg}.avif"' _ {} \;
+# Resize images to max 740px width
+find ./content -type f -name '*.png' -exec sh -c 'mogrify -resize 740x $1' _ {} \;
+find ./content -type f -name '*.jpg' -exec sh -c 'mogrify -resize 740x $1' _ {} \;
+find ./content -type f -name '*.jpeg' -exec sh -c 'mogrify -resize 740x $1' _ {} \;
+# Convert all images to webp
+find ./content -type f -name '*.png' -exec sh -c 'cwebp $1 -o "${1%.png}.webp"' _ {} \;
+find ./content -type f -name '*.jpg' -exec sh -c 'cwebp $1 -o "${1%.jpg}.webp"' _ {} \;
+find ./content -type f -name '*.jpeg' -exec sh -c 'cwebp $1 -o "${1%.jpeg}.webp"' _ {} \;
+# Convert all images to avif
+find ./content -type f -name '*.png' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.png}.avif"' _ {} \;
+find ./content -type f -name '*.jpg' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.jpg}.avif"' _ {} \;
+find ./content -type f -name '*.jpeg' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.jpeg}.avif"' _ {} \;
